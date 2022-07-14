08766a18210c956ce6640f71abf37587c96c352b
2over12 <ian.smith@trailofbits.com>
2022-07-14 17:06:18 -0400

    refine weak integer by struct field size (#14)

--- plugin/src/main/java/binary_type_inference/IUnknownTypeBuilder.java
+++ plugin/src/main/java/binary_type_inference/IUnknownTypeBuilder.java
@@ -1,6 +1,7 @@
 package binary_type_inference;
 
 import ghidra.program.model.data.DataType;
+import ghidra.program.model.data.IntegerDataType;
 
 public interface IUnknownTypeBuilder {
   public DataType getDefaultUnkownType();
@@ -8,10 +9,15 @@ public interface IUnknownTypeBuilder {
   default DataType refineDataTypeWithSize(DataType orig, int new_size) {
     if (this.getDefaultUnkownType().equals(orig)) {
       return this.getUnknownDataTypeWithSize(new_size);
+    } else if (IntegerDataType.dataType.equals(orig)) {
+      return this.getWeakestIntegerTypeWithSize(new_size);
     } else {
+
       return orig;
     }
   }
+
+  public DataType getWeakestIntegerTypeWithSize(int new_size);
 
   public DataType getUnknownDataTypeWithSize(int new_size);
 }
