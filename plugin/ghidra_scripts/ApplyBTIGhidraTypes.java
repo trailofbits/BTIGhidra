aaff9beaa1041d550905ac56f9ae89a260f27d2e
2over12 <ian.smith@trailofbits.com>
2022-09-30 16:47:11 -0400

    rethrow errors, add apply types script entry point

---
+++ plugin/ghidra_scripts/ApplyBTIGhidraTypes.java
@@ -0,0 +1,18 @@
+import binary_type_inference.BinaryTypeInference;
+import binary_type_inference.PreservedFunctionList;
+import ghidra.app.script.GhidraScript;
+import java.nio.file.Path;
+import java.util.ArrayList;
+
+public class ApplyBTIGhidraTypes extends GhidraScript {
+  @Override
+  protected void run() throws Exception {
+
+    var bti = new BinaryTypeInference(
+        currentProgram,
+        PreservedFunctionList.createFromExternSection(currentProgram, true),
+        new ArrayList<>(), null, false);
+
+    bti.run();
+  }
+}
