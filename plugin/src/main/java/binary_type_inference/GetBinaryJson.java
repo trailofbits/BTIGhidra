f8c807d7799ee064f2c0aa10c8f018c8e149569b
2over12 <ian.smith@trailofbits.com>
2022-10-13 14:54:53 -0400

    select entrypoint options

--- plugin/src/main/java/binary_type_inference/GetBinaryJson.java
+++ plugin/src/main/java/binary_type_inference/GetBinaryJson.java
@@ -8,6 +8,7 @@ import ghidra.app.script.GhidraScriptUtil;
 import ghidra.app.script.GhidraState;
 import ghidra.framework.model.Project;
 import ghidra.framework.plugintool.PluginTool;
+import ghidra.program.model.listing.Function;
 import ghidra.program.model.listing.Program;
 import ghidra.program.util.ProgramLocation;
 import ghidra.program.util.ProgramSelection;
@@ -15,8 +16,11 @@ import ghidra.util.Msg;
 import ghidra.util.task.TaskMonitor;
 import java.io.PrintWriter;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
+import java.util.Set;
 
 public class GetBinaryJson {
 
@@ -27,51 +31,66 @@ public class GetBinaryJson {
   private final ProgramSelection highlight;
   private final PluginTool tool;
   private final ResourceFile pcodeExtractorScript;
+  private Optional<List<Function>> target_functions;
 
-  public GetBinaryJson(
-      PluginTool tool,
-      Program prog,
-      Project project,
-      ProgramLocation loc,
-      ProgramSelection sel,
-      ProgramSelection highlight,
-      List<String> extra_script_dirs) {
+  public GetBinaryJson(PluginTool tool, Program prog, Project project,
+                       ProgramLocation loc, ProgramSelection sel,
+                       ProgramSelection highlight,
+                       List<String> extra_script_dirs,
+                       Optional<Set<Function>> target_functions) {
     this.prog = prog;
     this.project = project;
     this.loc = loc;
     this.sel = sel;
     this.highlight = highlight;
     this.tool = tool;
+    this.target_functions = target_functions.map((Set<Function> funcs) -> {
+      ArrayList<Function> list = new ArrayList<>();
+      list.addAll(funcs);
+      return list;
+    });
 
     if (GhidraScriptUtil.getBundleHost() == null) {
       GhidraScriptUtil.initialize(new BundleHost(), extra_script_dirs);
     }
 
-    this.pcodeExtractorScript = GhidraScriptUtil.findScriptByName("PcodeExtractor");
+    this.pcodeExtractorScript =
+        GhidraScriptUtil.findScriptByName("PcodeExtractor");
     Objects.requireNonNull(this.pcodeExtractorScript);
   }
 
+  public GetBinaryJson(PluginTool tool, Program prog, Project project,
+                       ProgramLocation loc, ProgramSelection sel,
+                       ProgramSelection highlight,
+                       List<String> extra_script_dirs) {
+    this(tool, prog, project, loc, sel, highlight, extra_script_dirs,
+         Optional.empty());
+  }
+
   void generateJSONIR(Path target_out) throws Exception {
-    GhidraScriptProvider provider = GhidraScriptUtil.getProvider(this.pcodeExtractorScript);
-    GhidraScript script =
-        provider.getScriptInstance(this.pcodeExtractorScript, new PrintWriter(System.err));
+    GhidraScriptProvider provider =
+        GhidraScriptUtil.getProvider(this.pcodeExtractorScript);
+    GhidraScript script = provider.getScriptInstance(
+        this.pcodeExtractorScript, new PrintWriter(System.err));
     String[] args = {target_out.toString()};
     script.setScriptArgs(args);
 
     ResourceFile srcFile = script.getSourceFile();
-    String scriptName =
-        srcFile != null ? srcFile.getAbsolutePath() : (script.getClass().getName() + ".class");
+    String scriptName = srcFile != null
+                            ? srcFile.getAbsolutePath()
+                            : (script.getClass().getName() + ".class");
     var scriptState = new GhidraState(tool, project, prog, loc, sel, highlight);
+    if (this.target_functions.isPresent()) {
+      scriptState.addEnvironmentVar("TARGET_FUNCTION_LIST",
+                                    this.target_functions.get());
+    }
     try {
-      script.execute(scriptState, TaskMonitor.DUMMY, new PrintWriter(System.out));
+      script.execute(scriptState, TaskMonitor.DUMMY,
+                     new PrintWriter(System.out));
     } catch (Exception exc) {
-      String logErrorMsg =
-          "REPORT SCRIPT ERROR: \""
-              + prog.getExecutablePath()
-              + "\" "
-              + scriptName
-              + " : "
-              + exc.getMessage();
+      String logErrorMsg = "REPORT SCRIPT ERROR: \"" +
+                           prog.getExecutablePath() + "\" " + scriptName +
+                           " : " + exc.getMessage();
       Msg.error(this, logErrorMsg, exc);
       throw exc;
     }
--- plugin/src/main/java/binary_type_inference/TypeAnalyzer.java
+++ plugin/src/main/java/binary_type_inference/TypeAnalyzer.java
@@ -5,20 +5,27 @@ import ghidra.app.services.AnalyzerType;
 import ghidra.app.util.importer.MessageLog;
 import ghidra.framework.options.OptionType;
 import ghidra.framework.options.Options;
+import ghidra.program.model.address.Address;
 import ghidra.program.model.address.AddressSetView;
+import ghidra.program.model.listing.Function;
 import ghidra.program.model.listing.Program;
 import ghidra.util.exception.CancelledException;
 import ghidra.util.task.TaskMonitor;
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 class TypeAnalyzerOptions {
   public Optional<File> preserved_functions_file;
   public boolean should_save_output;
   public boolean should_preserve_user_types;
   public boolean use_aggressive_shared_returns;
+  public Optional<String> entrypoints;
 
   public TypeAnalyzerOptions() {
     this.preserved_functions_file = Optional.empty();
@@ -27,9 +34,7 @@ class TypeAnalyzerOptions {
     this.use_aggressive_shared_returns = false;
   }
 
-  void setShouldSaveOutput(boolean val) {
-    this.should_save_output = val;
-  }
+  void setShouldSaveOutput(boolean val) { this.should_save_output = val; }
 
   void setPreservedFunctionsFile(File pth) {
     this.preserved_functions_file = Optional.of(pth);
@@ -47,7 +52,8 @@ public class TypeAnalyzer extends AbstractAnalyzer {
   private final List<String> extra_script_dir_paths = new ArrayList<>();
 
   public TypeAnalyzer() {
-    super("Type inference", "Analyzes program for types", AnalyzerType.BYTE_ANALYZER);
+    super("Type inference", "Analyzes program for types",
+          AnalyzerType.BYTE_ANALYZER);
     opts = new TypeAnalyzerOptions();
     this.setSupportsOneTimeAnalysis();
   }
@@ -70,10 +76,17 @@ public class TypeAnalyzer extends AbstractAnalyzer {
   @Override
   public void optionsChanged(Options options, Program program) {
     var file = options.getFile("Assume function list", null);
-    var new_bool = options.getBoolean("Save to debug directory", this.opts.should_save_output);
+    var new_bool = options.getBoolean("Save to debug directory",
+                                      this.opts.should_save_output);
+
+    var new_entry_point_list = options.getString("Entry points list", null);
+    if (new_entry_point_list != null) {
+      this.opts.entrypoints = Optional.of(new_entry_point_list);
+    }
+
     this.opts.setShouldSaveOutput(new_bool);
-    this.opts.should_preserve_user_types =
-        options.getBoolean("Preserve user defined types", this.opts.should_preserve_user_types);
+    this.opts.should_preserve_user_types = options.getBoolean(
+        "Preserve user defined types", this.opts.should_preserve_user_types);
     if (file != null) {
       this.opts.setPreservedFunctionsFile(file);
     } else {
@@ -81,9 +94,18 @@ public class TypeAnalyzer extends AbstractAnalyzer {
     }
 
     this.opts.use_aggressive_shared_returns =
-        options.getBoolean(
-            "Use aggressive shared returns (EXPERIMENTAL)",
-            this.opts.use_aggressive_shared_returns);
+        options.getBoolean("Use aggressive shared returns (EXPERIMENTAL)",
+                           this.opts.use_aggressive_shared_returns);
+  }
+
+  private static Set<Function> ParseEntryPointList(String list, Program prog) {
+    var addrs = list.split(";");
+    return Arrays.stream(addrs)
+        .map((String addr) -> prog.getAddressFactory().getAddress(addr))
+        .filter(Objects::nonNull)
+        .map((Address addr) -> prog.getFunctionManager().getFunctionAt(addr))
+        .filter(Objects::nonNull)
+        .collect(Collectors.toSet());
   }
 
   @Override
@@ -91,63 +113,54 @@ public class TypeAnalyzer extends AbstractAnalyzer {
 
     // TODO: If this analyzer has custom options, register them here
 
-    options.registerOption(
-        "Assume function list",
-        OptionType.FILE_TYPE,
-        null,
-        null,
-        "the function signatures that are assumed correct");
+    options.registerOption("Assume function list", OptionType.FILE_TYPE, null,
+                           null,
+                           "the function signatures that are assumed correct");
+
+    options.registerOption("Entry points list", OptionType.STRING_TYPE, null,
+                           "List of entry points in the format \"addr;addr\"");
 
     options.registerOption(
-        "Save to debug directory",
-        this.opts.should_save_output,
-        null,
+        "Save to debug directory", this.opts.should_save_output, null,
         "Saves intermediate artifacts instead of removing them after inference.");
 
     options.registerOption(
-        "Preserve user defined types",
-        this.opts.should_preserve_user_types,
+        "Preserve user defined types", this.opts.should_preserve_user_types,
         null,
         "If true, will add user defined function signatures to the assumed types.");
 
     options.registerOption(
         "Use aggressive shared returns (EXPERIMENTAL)",
-        this.opts.use_aggressive_shared_returns,
-        null,
+        this.opts.use_aggressive_shared_returns, null,
         "Use VSA to try to identify and fixup shared returns... currently high false positive rate");
   }
 
   @Override
-  public boolean added(Program program, AddressSetView set, TaskMonitor monitor, MessageLog log)
-      throws CancelledException {
+  public boolean added(Program program, AddressSetView set, TaskMonitor monitor,
+                       MessageLog log) throws CancelledException {
 
     // TODO: Perform analysis when things get added to the 'program'. Return
     // true if the analysis succeeded.
 
     Optional<PreservedFunctionList> maybe_preserved = Optional.empty();
     if (this.opts.preserved_functions_file.isPresent()) {
-      maybe_preserved =
-          PreservedFunctionList.parseTargetFunctionListFile(
-              program, this.opts.preserved_functions_file.get());
+      maybe_preserved = PreservedFunctionList.parseTargetFunctionListFile(
+          program, this.opts.preserved_functions_file.get());
     }
 
     if (maybe_preserved.isEmpty()) {
       maybe_preserved =
-          Optional.of(
-              PreservedFunctionList.createFromExternSection(
-                  program, this.opts.should_preserve_user_types));
+          Optional.of(PreservedFunctionList.createFromExternSection(
+              program, this.opts.should_preserve_user_types));
     }
 
     var preserved = maybe_preserved.get();
 
-    var bti =
-        new BinaryTypeInference(
-            program,
-            preserved,
-            this.extra_script_dir_paths,
-            log,
-            this.opts.should_save_output,
-            this.opts.use_aggressive_shared_returns);
+    var bti = new BinaryTypeInference(
+        program, preserved, this.extra_script_dir_paths, log,
+        this.opts.should_save_output, this.opts.use_aggressive_shared_returns,
+        this.opts.entrypoints.map(
+            (String ents) -> ParseEntryPointList(ents, program)));
 
     try {
       bti.run();
